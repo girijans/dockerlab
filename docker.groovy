0ed65079335bdfb5a483a0c39e9c00dc7db6bee1
girijans <108673476+girijans@users.noreply.github.com>
2022-07-30 07:22:06 +0530

    Update docker.groovy
    updated braces

--- docker.groovy
+++ docker.groovy
@@ -12,7 +12,7 @@ pipeline {
               steps {
                   sh """docker run -d --name sample -p 3000:3000 nodejstest:latest 
                         curl localhost:3000
-                        export status = $?
+                        export status = ${?}
                         if [ "${status}" != "0" ]
                         then
                           exit 
