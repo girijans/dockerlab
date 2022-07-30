ee812b9dfff9813755a45033dc30d91be0e453e8
girijans <108673476+girijans@users.noreply.github.com>
2022-07-30 16:57:55 +0530

    Update docker.groovy

--- docker.groovy
+++ docker.groovy
@@ -12,23 +12,14 @@ pipeline {
           stage ('Test') {
               environment { STATUS = ""  }
               steps {
-                  sh """docker run -d --name sample -p 3000:3000 nodejstest:latest
-                  #script {
-                       #final String url = "http://localhost:8080"
-                       #final String response = sh(script: "curl -s $url", returnStdout: true).trim()
-                       #echo response
-                  #}
-                       
+                  sh """docker run -d --name sample -p 3000:3000 nodejstest:latest              
                         curl localhost:3000
                         env.STATUS = \${?}
                         if [ "${env.STATUS}" != "0" ]
                         then
                           exit 
                         fi
-                        """
-                        # if [ echo ${} = 0 ]
-                        # exit
-                       
+                    """
                   }
               }
           
